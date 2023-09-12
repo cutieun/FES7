e859f5fe6acf1fdb569b68c6e1709e783b4cb907
cutieun <rich.jieunlee@gmail.com>
2023-09-12 22:20:47 +0900

    add modal component

--- React/20230911/componentlist/src/App3.js
+++ React/20230911/componentlist/src/App3.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import Login from './Components/Login';
 import Homepage from './Components/Homepage';
+import Modal from './Components/Modal';
 
 export default function App3() {
     const user = {
@@ -10,8 +11,14 @@ export default function App3() {
 
     // 로그인 상태를 판단하는 state
     const [login, setLogin] = useState(false);
+    // 모달이 보여지는 여부를 판단하는 state
+    const [modalShow, setModalShow] = useState(true);
 
     return (
-        login ? <Homepage/> : <Login infoUser={user} setLogin = {setLogin}/>
+        <>
+            {login ? <Homepage setLogin = {setLogin}/> : <Login infoUser={user} setLogin = {setLogin}/>}
+            {modalShow && <Modal setModalShow={setModalShow}/> }
+            
+        </>
     );
 }
--- React/20230911/componentlist/src/Components/Homepage.jsx
+++ React/20230911/componentlist/src/Components/Homepage.jsx
@@ -1,8 +1,8 @@
-function Homepage() {
+function Homepage({setLogin}) {
     return (
         <main>
             <h1>홈페이지에 오신것을 환영합니다!</h1>
-            <button>로그아웃</button>
+            <button onClick={() => setLogin(false)}>로그아웃</button>
         </main>
     )
 }
